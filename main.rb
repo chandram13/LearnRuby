292eea96a56d52e1e08c63a588891700bdb283c4
chandram13 <mschandr@uci.edu>
2022-08-11 13:04:31 -0700

    Initial commit

---
+++ main.rb
@@ -0,0 +1,12 @@
+
+
+print("Learning Ruby.")
+
+myCartoons = {"Cartoon Network" => "Flapback","Superheroes" => "Teen Titans", "Virtual Reality" => "Code Lyoko"}
+
+myCartoons["Cartoon Network"] = "Chowder"
+
+favoriteCartoons db: create do
+  "Naruto Shippuden"
+    "Dragon Ball Super"
+end
